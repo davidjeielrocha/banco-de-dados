9ead11638fbb3de4b7e77f923d9d182ea6cd56ca
davidjeielrocha <david.jeiel@iesb.edu.br>
2024-08-12 08:44:33 -0300

    Ultima resposta

--- MoneyMakers.session.sql
+++ MoneyMakers.session.sql
@@ -1,1 +1,6 @@
-SELECT * FROM artist_country_continent
+select t2.country,
+       sum(t1.streaming) quantidade
+  from money_makers t1 
+  left JOIN artist_country_continent t2
+    ON( t1.artist = t2.artist )
+  group BY country
